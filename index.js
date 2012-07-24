2899d3fc088031aaac1518db7e49b1aaa6d632b9
Matt Baker <matt.baker@airbnb.com>
2012-07-24 12:35:56 -0700

    save-to-puglist

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 !function(window, $, infinity, Pugs) {
   var ListView = infinity.ListView,
       ListItem = infinity.ListItem,
-      _ = require('underscore');
+      _ = require('underscore'),
+      modal = require('o2-modal');
 
   var template = _.template($('#demo-template').html()),
-      pugTemplate = _.template($('#demo-pug-template').html());
+      pugTemplate = _.template($('#demo-pug-template').html()),
+      spinnerTemplate = _.template($('#spinner-template').html());
 
   var listView = new ListView({
     className: 'list-view',
@@ -15,8 +17,47 @@
   });
   listView.appendTo($('#demo'));
 
+  var currPug = null;
+  $(document).on('click', '.modal .save', function() {
+    if(currPug) currPug.addClass('saved');
+    modal.close();
+    return false;
+  });
+  $(document).on('click', '.modal .no-save', function() {
+    currPug = null;
+    modal.close();
+    return false;
+  });
+  $(document).on('click', '.heart', function() {
+    currPug = $(this);
+    modal('#save-modal').open();
+    return false;
+  });
+
+  var pugCount = 0;
+
+  !function() {
+    var spinner = $(spinnerTemplate());
+    var updateScheduled = false;
+    function onscreen($el) {
+      var viewportBottom = $(window).scrollTop() + $(window).height();
+      return $el.offset().top <= viewportBottom;
+    }
+    spinner.insertAfter($('#demo').closest('.row'));
+    $(window).on('scroll', function() {
+      if(!updateScheduled) {
+        setTimeout(function() {
+          if(onscreen(spinner)) pb(200);
+          updateScheduled = false;
+        }, 500);
+        updateScheduled = true;
+      }
+    });
+  }();
+
   function pug(num) {
     var pugs = [];
+    pugCount += num;
     for(var index = 0; index < num; index++) {
       pugs.push(pugTemplate({
         num: num,
@@ -34,12 +75,16 @@
     });
   }
 
-  for(var i = 50; i > 0; i--) {
-    listView.append(row(Math.floor(Math.random() * 2) + 2));
+  function pb(num) {
+    for(var i = num; i > 0; i--) {
+      listView.append(row(Math.floor(Math.random() * 2) + 2));
+    }
   }
+
+  pb(200);
 
   window._debugListView = function() {
     return listView;
   }
-  console.log(listView);
+  window.pugCount = function() { return pugCount; };
 }(window, jQuery, infinity, Pugs);
