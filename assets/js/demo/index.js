befeb1411f6ccfacd192a6e2acca78af5fdb00c9
Matt Baker <matt.baker@airbnb.com>
2012-08-13 11:56:46 -0700

    better organization of stuff

--- assets/js/demo/index.js
+++ assets/js/demo/index.js
@@ -1,33 +1,12 @@
 !function(window, $, infinity, Pug) {
-  var ListView = infinity.ListView,
-      ListItem = infinity.ListItem,
-      _ = require('underscore'),
+  var _ = require('underscore'),
       modal = require('o2-modal'),
-      Pugs = Pug.images,
-      PugNames = Pug.names,
-      PugTaglines = Pug.taglines,
       PugStorage = Pug.storage;
 
-  var template = _.template($('#demo-template').html()),
-      pugTemplate = _.template($('#demo-pug-template').html()),
-      spinnerTemplate = _.template($('#spinner-template').html());
-
-  var columns = $('.list-view');
+  var spinnerTemplate = _.template($('#spinner-template').html());
 
   infinity.config.PAGE_TO_SCREEN_RATIO = 3;
   infinity.config.SCROLL_THROTTLE = 200;
-
-  columns.each(function() {
-    var listView = new ListView($(this), {
-      lazy: function() {
-        $(this).find('.pug').each(function() {
-          var $ref = $(this);
-          $ref.attr('src', $ref.attr('data-original'));
-        });
-      }
-    });
-    $(this).data('listView', listView);
-  });
 
   var currPug = null;
   $(document).on('click', '.modal .save', function() {
@@ -54,7 +33,6 @@
     return false;
   });
 
-  var pugCount = 0;
 
   !function() {
     var spinner = $(spinnerTemplate());
@@ -67,7 +45,7 @@
     $(window).on('scroll', function() {
       if(!updateScheduled) {
         setTimeout(function() {
-          if(onscreen(spinner)) pb(100);
+          if(onscreen(spinner)) Pug.bomb(100);
           updateScheduled = false;
         }, 500);
         updateScheduled = true;
@@ -75,59 +53,6 @@
     });
   }();
 
-  function pug() {
-    var rotate, rotateRight, rotateLeft, name, caption, pugData;
-    pugCount++;
 
-    tagline = _.template(PugTaglines[
-      Math.floor(Math.random() * PugTaglines.length)
-    ]);
-    name = PugNames[Math.floor(Math.random() * PugNames.length)];
-    caption = tagline({name: name});
-    pugData = Pugs[Math.floor(Math.random() * Pugs.length)];
-
-    rotate = Math.random() > 0.5;
-    rotateRight = rotate && Math.random() > 0.5;
-    rotateLeft = rotate && !rotateRight;
-    saved = PugStorage.check(pugData.src, name, caption);
-
-    return pugTemplate({
-      pug: pugData,
-      title: name,
-      caption: caption,
-      saved: saved,
-      price: Math.floor(Math.random() * 100 + 10),
-      rotateRight: rotateRight,
-      rotateLeft: rotateLeft
-    });
-  }
-
-
-  function row(num) {
-    var index, colIndex, length, minCol, currCol;
-
-    for(index = 0; index < num; index++) {
-
-      for(colIndex = 0, length = columns.length; colIndex < length; colIndex++) {
-        currCol = $(columns[colIndex]);
-        if(!minCol) minCol = currCol;
-        else minCol = minCol.height() > currCol.height() ? currCol : minCol;
-      }
-
-      minCol.data('listView').append(pug());
-    }
-  }
-
-  function pb(num) {
-    for(var i = num; i > 0; i--) {
-      row(3);
-    }
-  }
-
-  pb(200);
-
-  window._debugListView = function() {
-    return listView;
-  }
-  window.pugCount = function() { return pugCount; };
+  Pug.bomb(400);
 }(window, jQuery, infinity, Pug);
