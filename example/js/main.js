5bd52e4e6411eb6ce8cef039f3d98e19134d65cb
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2013-04-22 12:51:58 -0700

    A simple getting started infinity example and demo. "Turtles all the way down".

--- example/js/main.js
+++ example/js/main.js
@@ -7,16 +7,16 @@ $(document).ready(function() {
 //
 
 init=function(){
-	var mainList = $('#list-view');                //Get the container
-	listView = new infinity.ListView(mainList, {  //Inititalize infinity
-        lazy: function() {						          //With the lazy load callback
+  var mainList = $('#list-view');                //Get the container
+  listView = new infinity.ListView(mainList, {  //Inititalize infinity
+        lazy: function() {                      //With the lazy load callback
           $(this).find('.pic').each(function() {  
             var $ref = $(this);
             $ref.attr('src', $ref.attr('data-original')); //Set the img source from a string hard coded into the data-original attribute.
           });
         }
       });
-	mainList.data('listView', listView); //Use jQeary Data to set our list to the element as a conveniance.
+  mainList.data('listView', listView); //Use jQeary Data to set our list to the element as a conveniance.
   load(1000); //Load a 100 images!
 }
 
@@ -25,11 +25,11 @@ init=function(){
 //
 
 load=function(num){
-	var mainList = $('#list-view');
-	for (var i =  num- 1; i >= 0; i--) {
-		var html='<div> <img class="pic" data-original="https://www.google.com/images/srpr/logo4w.png" style="width: 312px; height: 100.208px;" ></img><div>'
-		mainList.data('listView').append( html )
-	};
+  var mainList = $('#list-view');
+  for (var i =  num- 1; i >= 0; i--) {
+    var html='<div> <a href="http://en.wikipedia.org/wiki/Turtles_all_the_way_down"> <img class="pic" data-original="http://farm5.staticflickr.com/4092/4985955642_01bc52672e.jpg" style="width: 512px; height: 360.208px;" ></img></a><div>'
+    mainList.data('listView').append( html )
+  };
 }
 
 
